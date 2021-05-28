e2ce439a53a9d30cf1e7ff76283434a041a6b345
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-05-28 17:27:24 -0400

    Move logo into public directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
 
-import logo from './logo.svg'
-
 function App () {
   const [count, setCount] = useState(0)
 
@@ -13,7 +11,7 @@ function App () {
   return (
     <div className="h-screen">
       <header className="h-full flex flex-col items-center justify-center">
-        <img src={logo} className="h-64 mb-12 animate-logo" alt="logo" />
+        <img src={'/logo.svg'} className="h-64 mb-12 animate-logo" alt="logo" />
         <p className="m-2">Edit <code className="bg-gray-300 px-2 py-1 rounded-md">src/App.svelte</code> and save to reload.</p>
         <p className="m-2">Page has been open for <code className="bg-gray-300 px-1 py-1 rounded-md">{count}</code> seconds.</p>
         <p className="m-2">
